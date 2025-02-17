71e547c36ccc8216dc5ebf97d1b1d05b16e2e20f
Valerie <britneyvalerie85@gmail.com>
2025-02-17 12:58:50 +0300

    changes added

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  
+        return
+    
+    fibonacci_sequence = [0, 1] if length > 1 else [0]  
+    
+    for _ in range(2, length):  
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])  
+    
+    print(fibonacci_sequence)
+
+
+
+      
